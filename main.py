709afdd6bc2c423ff8f3b7da1dcdab151e10f71c
Eganin <egorzaharin12@gmail.com>
2020-10-17 22:05:30 +0500

    end project

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+
+from cloudipsp import Api, Checkout
 from typing import *
 
 app: Flask = Flask(__name__)
 # указываем настройку какую БД будем использовать
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///shop.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db: SQLAlchemy = SQLAlchemy(app=app)
 
 
@@ -19,10 +22,16 @@ class Item(db.Model):
     # isActive default value = True
     isActive = db.Column(db.Boolean, nullable=True)
 
+    def __repr__(self) -> str:
+        return f"Запись : {self.title}"
+
 
 @app.route("/")
 def index() -> str:
-    return render_template("index.html")
+    # получаем все данные из таблицы Item с сортировкой по цене
+    items: Item = Item.query.order_by(Item.price).all()
+    # отправляем данные на страницу
+    return render_template("index.html", data=items)
 
 
 @app.route("/about")
@@ -30,9 +39,48 @@ def about() -> str:
     return render_template("about.html")
 
 
-@app.route("/create")
+# на данной странице отслеживаем запросы POST
+@app.route("/create", methods=["POST", "GET"])
 def create() -> str:
-    return render_template("create.html")
+    if request.method == "POST":
+        # получаем отправленные данные
+        title: str = request.form["title"]
+        price: str = request.form["price"]
+
+        # запись в бд
+        item: Item = Item(title=title, price=price)
+
+        try:
+            # сохраняем запись в БД
+            db.session.add(item)
+            db.session.commit()
+            # делаем переадресацию на главную страницу
+            return redirect("/")
+        except:
+            return "ERROR"
+    else:
+        return render_template("create.html")
+
+
+# отследивание динамического URI
+@app.route("/buy/<int:id>")
+def item_buy(id: int):
+    # получаем item по id
+    item: Item = Item.query.get(id)
+
+    # всзимодействуем с  API для оплаты
+    api: Api = Api(merchant_id=1396424,
+                   secret_key="test")
+    checkout: Checkout = Checkout(api=api)
+    data: Dict[str, str] = {
+        "currency": "RUB",
+        "amount": str(item.price) + "00"
+    }
+
+    # url покупки товара
+    url: str = checkout.url(data).get("checkout_url")
+
+    return redirect(url)
 
 
 def main() -> None:
